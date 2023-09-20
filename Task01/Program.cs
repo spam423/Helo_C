0592be04987624d2684a6c59e23600f400820340
spam423 <zombe423@list.ru>
2023-09-20 10:12:14 +0300

    days of the week

---
+++ Task01/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Напишите программу, которая будет выдавать
+название дня недели по заданному номеру.
+3 -> среда
+5 -> пятница */
+
+string dayNumber;
+Console.WriteLine("Введите номер дня недели: ");
+dayNumber = Console.ReadLine();
+if (dayNumber == "1")
+{
+    Console.WriteLine($"{dayNumber} -> Понедельник");
+}
+else if (dayNumber == "2")
+{
+    Console.WriteLine($"{dayNumber} -> Вторник");
+}
+else if (dayNumber == "3")
+{
+    Console.WriteLine($"{dayNumber} -> Среда");
+}
+else if (dayNumber == "4")
+{
+    Console.WriteLine($"{dayNumber} -> Четверг");
+}
+else if (dayNumber == "5")
+{
+    Console.WriteLine($"{dayNumber} -> Пятница");
+}
+else if (dayNumber == "6")
+{
+    Console.WriteLine($"{dayNumber} -> Суббота");
+}
+else if (dayNumber == "7")
+{
+    Console.WriteLine($"{dayNumber} -> Воскресенье");
+}
+else
+{
+    Console.WriteLine("Дня недели с таким номером нет");
+}
